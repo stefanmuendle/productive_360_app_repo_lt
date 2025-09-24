50d4e74ccd37fbc87510d9cb28a83bd387ff98fc
stefanmuendle <131877125+stefanmuendle@users.noreply.github.com>
2025-09-24 15:06:18 +0200

    widget tree added without any other imports

---
+++ lib/data/notifiers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+//import 'package:productive_360_app/models/habit_collection.dart';
+
+ValueNotifier<int> selectedPageNotifier = ValueNotifier<int>(0);
+ValueNotifier<bool> isDarkModeNotifier = ValueNotifier<bool>(false);
+
+ValueNotifier<bool> isLoggedInNotifier = ValueNotifier<bool>(false);
+
+ValueNotifier<bool> preloadedHabitsNotifier = ValueNotifier<bool>(false);
+// ValueNotifier<HabitCollection> habitCollectionNotifier =
+//     ValueNotifier<HabitCollection>(HabitCollection());
+
+ValueNotifier<bool> habitsChangedNotifier = ValueNotifier<bool>(false);
---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,88 @@
+///import 'package:firebase_auth/firebase_auth.dart';
+library;
+
+import 'package:flutter/material.dart';
+
+// import 'package:productive_360_app/views/pages/dashboard_page.dart';
+// import 'package:productive_360_app/views/pages/login_page.dart';
+// import '../data/notifiers.dart';
+// import 'pages/calendar_page.dart';
+// import 'pages/habits_page.dart';
+// import 'pages/settings_page.dart';
+//import 'widgets/navbar.dart';
+
+//List<Widget> pages = [DashboardPage(), CalendarPage(), HabitsPage()];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.monitor_heart_sharp, color: Colors.blue, size: 38),
+            const SizedBox(width: 6),
+            const Text(
+              'P360',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+          ],
+        ),
+        actions: [
+          // StreamBuilder<User?>(
+          //   stream: FirebaseAuth.instance.authStateChanges(),
+          //   builder: (context, snapshot) {
+          //     final user = snapshot.data;
+          //     if (user == null) {
+          //       return TextButton(
+          //         onPressed: () {
+          //           // Navigator.push(
+          //           //   context,
+          //           //   MaterialPageRoute(
+          //           //     builder: (context) => const LoginPage(title: 'Login'),
+          //           //   ),
+          //           // );
+          //         },
+          //         child: Text('Login'),
+          //       );
+          //     } else {
+          //       // return IconButton(
+          //       //   icon: const Icon(Icons.logout, color: Colors.red),
+          //       //   tooltip: 'Logout',
+          //       //   onPressed: () async {
+          //       //     await FirebaseAuth.instance.signOut();
+          //       //   },
+          //       // );
+          //     }
+          //   },
+          // ),
+          IconButton(
+            icon: const Icon(Icons.star, color: Colors.amber),
+            onPressed: () {
+              // TODO: Add star action
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings, color: Colors.grey),
+            onPressed: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const SettingsPage()),
+              // );
+            },
+          ),
+        ],
+      ),
+      // body: ValueListenableBuilder(
+      //   valueListenable: selectedPageNotifier,
+      //   builder: (context, selectedPage, child) {
+      //     return pages.elementAt(selectedPage);
+      //   },
+      // ),
+      //bottomNavigationBar: const NavbarWidget(),
+    );
+  }
+}
